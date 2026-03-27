0870512820a74754c0e91e979d61dd91f3329eda
Nicolas <n178372@gmail.com>
2026-03-27 09:51:23 -0300

    feat: declarações e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// DECLARAÇÕES E VARIAVEIS
+//VAR, LET E CONST
+
+let nome="Fiap";
+console.log(nome);
+let nome2="Dev"
+console.log(nome2);
+
+if(true){
+    var avatar="Fiapinho";
+}
+
+console.log(avatar);
